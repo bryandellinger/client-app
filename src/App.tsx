3bdcc691d8b0722d8ba6e32d7d1ba610b82ca4f8
Bryan Dellinger <dellingerfam5@gmail.com>
2022-08-31 10:33:04 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Providers, ProviderState } from '@microsoft/mgt';
+import { Login, Person, Agenda, PeoplePicker, People } from '@microsoft/mgt-react'; // import login component
+import { Header, List } from 'semantic-ui-react';
+
+
+function useIsSignedIn(): [boolean] {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  useEffect(() => {
+    const updateState = () => {
+      const provider = Providers.globalProvider;
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+    };
+
+    Providers.onProviderUpdated(updateState);
+    updateState();
+
+    return () => {
+      Providers.removeProviderUpdatedListener(updateState);
+    }
+  }, []);
+
+  return [isSignedIn];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [isSignedIn] = useIsSignedIn();
+const [people, setPeople] = useState([]);
+const [activities, setActivities] = useState([]);
+
+const getActivities = () => {
+  if (Providers.globalProvider.state === ProviderState.SignedIn) {
+    const provider = Providers.globalProvider;
+    let graphClient = provider.graph.client;
+    graphClient.api('/groups/88d59881-7b15-4adc-a756-5d10681cf99d/calendar/events').get()
+    .then(response => {
+      console.log(response);
+      setActivities(response.value)
+    })
+    }
+}
+
+const providerStateChanged = () => {
+  console.log('provider State Changed');
+    getActivities();
+  }
+
+useEffect(() => {
+  console.log('use effect');
+  getActivities();
+},[])
+
+Providers.onProviderUpdated(providerStateChanged);
+
+const handleSelectionChanged = (e: any) => {
+  setPeople(e.target.selectedPeople);
+};
+
+
+return (
+  <div>
+    <Header as='h1' textAlign='right'>
+      <Login />
+    </Header>
+    {/*
+  <header>
+  <Login />
+  </header>
+*/}
+  {isSignedIn &&
+    <div>
+     { /*<div>
+        <p>Person Component : <Person personQuery="me"></Person></p>
+      </div>
+      <div style={{ width: "400px" }}>
+        <p>People Picker Component : <PeoplePicker selectionChanged={handleSelectionChanged} /></p>
+        <p>People Component: <People people={people} /></p>
+      </div>
+      <div>
+      <Agenda />
+      </div>*/}
+      <List>
+      {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.subject}
+            </List.Item>
+        ))}
+      </List>
+     
+  </div> }
+</div>
+);
 }
 
 export default App;
